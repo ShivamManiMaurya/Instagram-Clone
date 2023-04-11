b86aa799d5ec86dc046b134319b1f3c9753c0d66
Shivam Maurya <109658264+ShivamManiMaurya@users.noreply.github.com>
2023-04-11 11:18:31 +0530

    materialUI intalled and avatar added to post

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+// Styles
 import './App.css';
+
+// Components
+import Post from './component/Post';
 
 function App() {
   return (
     <div className="App">
+      
+      <div className="app__header">
+        <img 
+          className='app__headerImg'
+          src="https://1000logos.net/wp-content/uploads/2017/02/Logo-Instagram.png" 
+          alt="Instagram-logo" 
+        />
+      </div>
+
       <h1>Instagram clone app is in progress</h1>
+
+
+      {/* header */}
+      {/* Post */}
+      <Post />
+      {/* post */}
+
     </div>
   );
 }
--- src/component/Post.js
+++ src/component/Post.js
@@ -1,9 +1,30 @@
-import React from 'react'
+// Styles
+import './styles/Post.css';
+
+// component
+import React from 'react';
+import Avatar from '@mui/material/Avatar';
+
+
 
 function Post() {
   return (
-    <div>
-        
+    <div className="post">
+        {/* header -> avatar + username */}
+        <div className="post___header">
+            <Avatar />
+            <h3 className='post__headerUsername'>MS Dhoni</h3>
+        </div>
+
+
+        {/* post-img  */}
+        <img 
+            className='post__img'
+            src="https://images.successstory.com/img_people/profile/620Xauto/profile5_1438946298.jpg" 
+            alt="" 
+        />
+        {/* username + caption  */}
+        <h4 className="post__caption">MS Dhoni <span>Mai pal do pal ka shayar hun</span></h4>
     </div>
   )
 }
